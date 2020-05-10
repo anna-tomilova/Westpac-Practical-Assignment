6eac0f01164a416ec57a8b89d1453df2642609b4
Anna Tomilova <Ana.tomilova@gmail.com>
2020-05-10 13:15:55 +1200

    Add project files.

---
+++ Selenium Assignment/SeleniumAssignment.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Assignment
+{
+    class SeleniumAssignment
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.westpac.co.nz/";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
