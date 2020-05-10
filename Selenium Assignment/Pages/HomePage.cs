5d0c4b36e26b1af2082eb7299d14e451e3237462
Anna Tomilova <Ana.tomilova@gmail.com>
2020-05-10 16:21:44 +1200

    high level structure

---
+++ Selenium Assignment/Pages/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Assignment.Pages
+{
+    public class HomePage
+    {
+        IWebDriver driver;
+        By kiwisaverNavTab = By.Id("ubermenu-section-link-kiwisaver-ps");
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;    
+        }
+
+        //Click Kiwisaver menu tab
+        public void clickKiwisaver()
+        {
+            driver.FindElement(kiwisaverNavTab).Click();
+        }
+    }
+
+}
